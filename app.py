f7ede2cca9a7101728289d9d2903e56a90787767
HD599 <csansamak@gmail.com>
2025-09-28 11:20:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import time
-import os
+
 
 app = Flask(__name__)
 
@@ -38,5 +38,5 @@ def result():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+
