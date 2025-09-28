574161deaedaaf795a1d6111454cb8bbc1a759db
HD599 <csansamak@gmail.com>
2025-09-28 16:36:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import time
+import os
+
 
 app = Flask(__name__)
 app.secret_key = "secret123"  # ใช้สำหรับ session
@@ -60,6 +63,7 @@ def result():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
